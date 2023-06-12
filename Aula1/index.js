b4140fd0c40d39508b050102579e432bea12dbf9
Gabi Gio <gabrielagiovanna3007@gmail.com>
2023-06-12 09:49:39 -0300

    Add files via upload

---
+++ Aula1/index.js
@@ -0,0 +1,6 @@
+console.log("O resultado é ",5+2)
+
+const gabis = 1 /* Uma constante não muda nunca(não pode ser mudado)mais leve */
+let gabi = 5  /* Pode ser modificado e etc() */
+
+console.log("A soma deu ",gabi + gabis)
